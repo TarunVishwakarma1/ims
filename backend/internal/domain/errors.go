e56a019ea1b8f0f7c785163f547426aebcc7d655
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-06-11 11:49:21 +0530

    Created custom errors (errors.go)
    updated category_repository.go and user_repository.go to use errors from domain package

---
+++ backend/internal/domain/errors.go
@@ -0,0 +1,12 @@
+package domain
+
+import "errors"
+
+var (
+	ErrNotFound     = errors.New("not found")
+	ErrConflict     = errors.New("already exist")
+	ErrUnauthorized = errors.New("unauthorized")
+	ErrBadRequest   = errors.New("bad request")
+	ErrInternal     = errors.New("internal error")
+	ErrInvalid      = errors.New("invalid")
+)
